3f105ac4c1ff2ba5d7521c5bff59a9302363f289
Florian <31819550+flo-kei@users.noreply.github.com>
2023-04-10 19:32:44 +0000

    implement backend logic

--- frontend2/src/pages/index.tsx
+++ frontend2/src/pages/index.tsx
@@ -2,6 +2,11 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 
+const login = () => {
+  console.log("login started");
+  
+}
+
 const Home: NextPage = () => {
   return (
     <>
@@ -15,7 +20,7 @@ const Home: NextPage = () => {
           <h1 className="justify-self-center">WebAuthn Demo</h1>
         </div>
         <div className="flex h-full items-center justify-center flex-col space-y-4">
-          <button className="btn w-1/6">Login</button>
+          <button className="btn w-1/6" onClick={() => login()}>Login</button>
           <button className="btn w-1/6">Logout</button>
         </div>
       </div>
